dc0b018848d7adfe9dcce27143820e8d0c7e73ae
Melissendra <melissendra@gmail.com>
2020-05-22 08:09:45 +0200

    select_data fils finished

--- select_datas.sql
+++ select_datas.sql
@@ -9,17 +9,23 @@ INNER JOIN public.category
 ON public.category.id = public.user.id_category
 WHERE public.category.category_name = 'client' OR public.category.category_name = 'manager';
 
-SELECT public.user.lastname, public.user.firstname, public.address.street_number, public.address.street_name, public.address.city FROM public.address
-INNER JOIN public.user
-ON public.user.id = public.address.id_user;
-
 SELECT public.user.lastname, public.user.firstname, public.order_state.preparation_state, public.restaurant.name FROM public.user
 INNER JOIN public.address
-ON public.address.id =  public.user.id
+ON public.address.id_user =  public.user.id
 INNER JOIN public.order
 ON public.order.id = public.address.id
 INNER JOIN public.order_state
 ON public.order_state.id = public.order.id
 INNER JOIN public.restaurant
-ON public.restaurant.id = public.user.id_restaurant;
+ON public.restaurant.id = public.order.id_restaurant;
+
+SELECT public.product.product_name, public.basket_line.basket_date, public.basket_line.product_amount, public.basket_line.price, public.user.lastname, public.user.firstname FROM public.product
+INNER JOIN public.basket_line
+ON public.basket_line.id_product = public.product.id
+INNER JOIN public.order
+ON public.order.id = public.basket_line.id_order
+INNER JOIN public.address
+ON public.address.id = public.order.id_address
+INNER JOIN public.user
+ON public.user.id = public.address.id_user;
 
